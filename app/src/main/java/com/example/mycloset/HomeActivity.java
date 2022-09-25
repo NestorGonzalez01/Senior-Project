5227e56d7c00cfef85b2fe4139876a92446d3e5d
NaughteaMonkey <104109964+NaughteaMonkey@users.noreply.github.com>
2022-09-25 14:52:08 -0500

    Burger is now functioning

--- app/src/main/java/com/example/mycloset/HomeActivity.java
+++ app/src/main/java/com/example/mycloset/HomeActivity.java
@@ -1,18 +1,36 @@
 package com.example.mycloset;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class HomeActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Toolbar toolbar = findViewById(R.id.mytoolbar);
+        //Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.mytoolbar);
+
+        //Toolbar
         setSupportActionBar(toolbar);
+
+        //Navigation drawer menu
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }

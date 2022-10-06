583f22ab82f12d0e29f370335bf93e332329c136
paolacibarra <paola.c.ibarra01@utrgv.edu>
2022-10-06 10:20:10 -0500

    viewcloset
    main page the tab "View Closet"

--- app/src/main/java/com/example/mycloset/Tops.java
+++ app/src/main/java/com/example/mycloset/Tops.java
@@ -1,17 +1,16 @@
 package com.example.mycloset;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ViewCloset extends AppCompatActivity {
+public class Tops extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_closet);
+        setContentView(R.layout.activity_tops);
     }
 
-}
 
+}

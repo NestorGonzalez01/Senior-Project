925329798641a7092e81cebc768b9cb42b4ed9a5
paolacibarra <paola.c.ibarra01@utrgv.edu>
2022-10-10 08:49:38 -0500

    Tabs
    Added clothing

--- app/src/main/java/com/example/mycloset/Bottoms.java
+++ app/src/main/java/com/example/mycloset/Bottoms.java
@@ -11,36 +11,22 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 
-public class Tops extends AppCompatActivity {
-    ImageButton imgButton;
+public class Bottoms extends AppCompatActivity {
     public Button button;
-
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tops);
+        setContentView(R.layout.activity_bottoms);
 
-        button = (Button) findViewById(R.id.button8);
+        button = (Button) findViewById(R.id.buttonBack);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Tops.this, ViewCloset.class);
+                Intent intent = new Intent(Bottoms.this, ViewCloset.class);
                 startActivity(intent);
             }
         });
-
-        imgButton = (ImageButton) findViewById(R.id.imageButton);
-        imgButton.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
     }
-
-
 }
--- app/src/main/java/com/example/mycloset/Shoes.java
+++ app/src/main/java/com/example/mycloset/Shoes.java
@@ -8,39 +8,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
-
-public class Tops extends AppCompatActivity {
-    ImageButton imgButton;
+public class Shoes extends AppCompatActivity {
     public Button button;
 
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tops);
+        setContentView(R.layout.activity_shoes);
 
-        button = (Button) findViewById(R.id.button8);
+        button = (Button) findViewById(R.id.buttonShoeBack);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Tops.this, ViewCloset.class);
+                Intent intent = new Intent(Shoes.this, ViewCloset.class);
                 startActivity(intent);
             }
         });
-
-        imgButton = (ImageButton) findViewById(R.id.imageButton);
-        imgButton.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
     }
-
-
 }

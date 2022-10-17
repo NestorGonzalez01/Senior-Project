d42c2eb42893c3b9101f9231110a837180e78e65
paolacibarra <paola.c.ibarra01@utrgv.edu>
2022-10-17 13:44:42 -0500

    tops
    changes

--- app/src/main/java/com/example/mycloset/Tops.java
+++ app/src/main/java/com/example/mycloset/Tops.java
@@ -24,8 +24,6 @@ public class Tops extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tops);
-         ImageButton image1 = findViewById(R.id.imageButton);
-         ImageButton image2 = findViewById(R.id.imageButton5);
          img  = (ImageView) findViewById(R.id.imageView2);
 
         button = (Button) findViewById(R.id.button8);
@@ -39,23 +37,6 @@ public class Tops extends AppCompatActivity {
             }
         });
 
-        ImageButton imageButton,imageButton2,imageButton3,imageButton4, imageButton5;
-
-        imageButton = findViewById(R.id.imageButton);
-        imageButton2 = findViewById(R.id.imageButton2);
-        imageButton3 = findViewById(R.id.imageButton3);
-        imageButton4 = findViewById(R.id.imageButton4);
-        imageButton5 = findViewById(R.id.imageButton5);
-        final RelativeLayout relativeLayout;
-
-        imageButton2.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Tops.this, ViewOutfits.class);
-                image2.setImageResource(R.drawable.tops5);
-            }
-        });
 
 
     }

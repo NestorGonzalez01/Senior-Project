60ded83559011d85460b64babea524ac42986d17
paolacibarra <paola.c.ibarra01@utrgv.edu>
2022-10-17 12:21:43 -0500

    viewcloset
    viewcloset

--- app/src/main/java/com/example/mycloset/Tops.java
+++ app/src/main/java/com/example/mycloset/Tops.java
@@ -9,47 +9,56 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 
 public class Tops extends AppCompatActivity {
     ImageButton imgButton;
     public Button button;
-
+    ImageView img;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tops);
+         ImageButton image1 = findViewById(R.id.imageButton);
+         ImageButton image2 = findViewById(R.id.imageButton5);
+         img  = (ImageView) findViewById(R.id.imageView2);
+
         button = (Button) findViewById(R.id.button8);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Tops.this, ViewCloset.class);
                 startActivity(intent);
+
+
             }
         });
 
-        imgButton = (ImageButton) findViewById(R.id.imageButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        ImageButton imageButton,imageButton2,imageButton3,imageButton4, imageButton5;
+
+        imageButton = findViewById(R.id.imageButton);
+        imageButton2 = findViewById(R.id.imageButton2);
+        imageButton3 = findViewById(R.id.imageButton3);
+        imageButton4 = findViewById(R.id.imageButton4);
+        imageButton5 = findViewById(R.id.imageButton5);
+        final RelativeLayout relativeLayout;
+
+        imageButton2.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Tops.this, ViewOutfits.class);
-                i.putExtra("resId", R.drawable.top1);
-                startActivity(i);
+                Intent intent = new Intent(Tops.this, ViewOutfits.class);
+                image2.setImageResource(R.drawable.tops5);
             }
         });
 
 
-
     }
-    }
-
-
-
-
-
-
-
+}
 
 
--- app/src/main/java/com/example/mycloset/ViewOutfits.java
+++ app/src/main/java/com/example/mycloset/ViewOutfits.java
@@ -4,27 +4,74 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 
 public class ViewOutfits extends AppCompatActivity {
-    ImageView i1;
+    ImageView image;
+    public Button button;
+    boolean image1Displaying = false;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_outfits);
-        i1 = (ImageView)findViewById(R.id.imageView2);
+        image = findViewById(R.id.imageView2);
+
+        button = (Button) findViewById(R.id.buttonBack1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ViewOutfits.this, ViewCloset.class);
+                startActivity(intent);
+            }
+        });
+
+       /* image = (ImageView) findViewById(R.id.imageView2);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+
+            int resId = bundle.getInt("image2");
+            image.setImageResource(resId);
+
+        }*/
+    }
+
+}
+
+
+       /* image.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(image1Displaying){
+                    image.setImageBitmap(BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.top1));
+
+                }else{
+                    image.setImageBitmap(BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.tops5));
+
+                }
+                return false;
+            }
+
+
+        });*/
+
+
+
+
+
+/* image = (ImageView)findViewById(R.id.imageView2);
 
         Bundle bundle = getIntent().getExtras();
         if(bundle!= null)
         {
             int resId = bundle.getInt("resId");
-            i1.setImageResource(resId);
-        }
-
-
-    }
-}
+            image.setImageResource(resId);
+        }*/

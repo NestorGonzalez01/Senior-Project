752776decb92d50c15d3f9bffd5d453c2a6587b0
NaughteaMonkey <104109964+NaughteaMonkey@users.noreply.github.com>
2022-12-04 14:26:34 -0600

    Style_2
    Style_2

--- app/src/main/java/com/example/mycloset/StyleMe.java
+++ app/src/main/java/com/example/mycloset/StyleMe.java
@@ -7,6 +7,8 @@ import android.util.Log;
 
 import com.example.mycloset.db.DBHandler;
 
+import java.util.Random;
+
 public class StyleMe extends AppCompatActivity {
 
     @Override
@@ -16,5 +18,8 @@ public class StyleMe extends AppCompatActivity {
 
         long s = new DBHandler(this).getNumEntries("tops");
         Log.d("size", "Table size is: " + s);
+
+        int n = new Random().nextInt((int)s+1);
+        Log.d("size", "Random number: " + n);
     }
 }

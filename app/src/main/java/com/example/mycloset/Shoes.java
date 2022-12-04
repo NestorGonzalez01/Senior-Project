efd1f1663b70367ff3176e74085124d4973457f9
paolacibarra <paola.c.ibarra01@utrgv.edu>
2022-12-04 15:41:37 -0600

    CreateOutfit feature array
    array was created where an image of each category is stored into an an array.

--- app/src/main/java/com/example/mycloset/Shoes.java
+++ app/src/main/java/com/example/mycloset/Shoes.java
@@ -8,9 +8,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Shoes extends AppCompatActivity {
     public Button button;
+    ImageButton imageButton1, imageButton2, imageButton3, imageButton4, imageButton5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +27,51 @@ public class Shoes extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        imageButton1 = findViewById(R.id.imageButton1);
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Shoes.this, Accessories.class);
+                startActivity(intent);
+            }
+        });
+        imageButton2 = findViewById(R.id.imageButton2);
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Shoes.this, Accessories.class);
+                startActivity(intent);
+            }
+        });
+        imageButton3 = findViewById(R.id.imageButton3);
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Shoes.this, Accessories.class);
+                startActivity(intent);
+            }
+        });
+
+
+        imageButton4 = findViewById(R.id.imageButton4);
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Shoes.this, Accessories.class);
+                startActivity(intent);
+            }
+        });
+
+        imageButton5 = findViewById(R.id.imageButton5);
+        imageButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Shoes.this, Accessories.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }

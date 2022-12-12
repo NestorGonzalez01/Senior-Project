d4be1d2427d16d12b6b0ec6274789b921e30589c
PatricioOliva21 <triciooliva@gmail.com>
2022-12-12 13:44:17 -0600

    Updated "CoMpLETed PrOJecT"
    did the Project and finished

--- app/src/main/java/com/example/mycloset/Bottoms.java
+++ app/src/main/java/com/example/mycloset/Bottoms.java
@@ -27,7 +27,7 @@ public class Bottoms extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shoes);
+        setContentView(R.layout.activity_bottoms);
 
         DBHandler db = new DBHandler(this);
         LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout);
